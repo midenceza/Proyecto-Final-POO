b003dee80a203418d4e78accf1b2afa0d303799d
midenceza <jose.midence14840@est.uca.edu.ni>
2022-11-21 00:10:32 -0600

    Primer Commit

---
+++ src/sistemaiglesia/Principal.java
@@ -0,0 +1,14 @@
+
+package sistemaiglesia;
+
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
